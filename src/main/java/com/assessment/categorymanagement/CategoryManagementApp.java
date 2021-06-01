e11298d42c2e1828b2d304d8d5016a8d599cff01
francis <francis.valero@nagarro.com>
2021-06-01 17:21:01 +0400

    initial commit + liquibase scripts

---
+++ src/main/java/com/assessment/categorymanagement/CategoryManagementApp.java
@@ -0,0 +1,13 @@
+package com.assessment.categorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryManagementApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryManagementApp.class, args);
+	}
+
+}
