0aa441089ac32dd9dd7ad887d5c4d4f5af7156ff
francis <francis.valero@nagarro.com>
2021-06-01 20:59:10 +0400

    Enable CORS

--- src/main/java/com/assessment/categorymanagement/config/WebConfig.java
+++ src/main/java/com/assessment/categorymanagement/config/WebConfig.java
@@ -3,13 +3,21 @@ package com.assessment.categorymanagement.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
